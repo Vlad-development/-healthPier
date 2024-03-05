90a0666ffecfdb2ef1b346b7ab213656035a7a7a
Dmitriy <prowizard.ru@gmail.com>
2024-03-05 02:25:53 +0200

    fix function

--- wp-content/themes/healthpier/functions.php
+++ wp-content/themes/healthpier/functions.php
@@ -212,3 +212,9 @@ function my_acf_indications_render_callback( $block ) {
         include( get_theme_file_path("/template-parts/block/{$slug}.php") );
     }
 }
+
+add_filter('wpcf7_autop_or_not', '__return_false');
+add_filter('wpcf7_form_elements', function($content) {
+    $content = preg_replace('/<(span).*?class="\s*(?:.*\s)?wpcf7-form-control-wrap(?:\s[^"]+)?\s*"[^\>]*>(.*)<\/\1>/i', '\2', $content);
+    return $content;
+});
