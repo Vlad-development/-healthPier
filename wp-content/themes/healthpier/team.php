ba435fc473282c66a016086497ece3c68a32957a
Dmitriy <prowizard.ru@gmail.com>
2024-03-05 02:54:06 +0200

    changed blocks

--- wp-content/themes/healthpier/team.php
+++ wp-content/themes/healthpier/team.php
@@ -72,47 +72,7 @@ Template Post Type:  page
         </section>
 
 
-        <section class="specialists">
-            <div class="container">
-                <div class="specialists__wrap">
-                    <div class="specialists-content">
-                        <h2 class="specialists-content__title h2">
-                            Остались вопросы? Наши специалисты с удовольствием вам помогут.
-                        </h2>
-                        <p class="specialists-content__text">
-                            Заполните форму ниже и мы свяжемся с вами в ближайшее время
-                        </p>
-                        <div class="specialists-form">
-                            <div class="specialists-form__fields">
-                                <label>
-                            <span>Имя</span>
-                            <input type="text" placeholder="Ирина">
-                        </label>
-                                <label>
-                            <span>Телефон</span>
-                            <input type="text" placeholder="+7 999 555 66 77">
-                        </label>
-                                <label>
-                            <span>E-mail</span>
-                            <input type="text" placeholder="Example@mail.com">
-                        </label>
-                            </div>
-                            <div class="specialists-form__checkbox">
-                                <label>
-                            <input type="checkbox">
-                            <span>Я даю согласие на обработку персональных данных</span>
-                        </label>
-                            </div>
-                            <button type="button" class="btn specialists-form__btn btn-main">Отправить</button>
-
-                        </div>
-                    </div>
-                    <div class="specialists__img">
-                        <img src="<?php bloginfo('template_directory') ?>/img/specialists-img.jpeg" alt="">
-                    </div>
-                </div>
-            </div>
-        </section>
+        <?php get_template_part('template-parts/specialists') ?>
     </main>
 
 
