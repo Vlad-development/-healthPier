3e5b4dc8c9d0f3212a19a0324deb69b192b54700
Dmitriy <prowizard.ru@gmail.com>
2024-03-06 23:16:36 +0200

    fix page team

--- wp-content/themes/healthpier/team.php
+++ wp-content/themes/healthpier/team.php
@@ -23,50 +23,34 @@ Template Post Type:  page
                 </h1>
 
                 <div class="team__list">
-                    <a href="#" class="team-card">
-                        <div class="team-card__img">
-                            <img src="<?php bloginfo('template_directory') ?>/img/team.jpeg" alt="">
-                        </div>
-                        <h3 class="team-card__title">
-                            Имя Отчество Фамилия
-                        </h3>
-                        <div class="team-card__description">
-                            Главный врач
-                        </div>
-                    </a>
-                    <a href="#" class="team-card">
-                        <div class="team-card__img">
-                            <img src="<?php bloginfo('template_directory') ?>/img/team.jpeg" alt="">
-                        </div>
-                        <h3 class="team-card__title">
-                            Имя Отчество Фамилия
-                        </h3>
-                        <div class="team-card__description">
-                            Главный врач
-                        </div>
-                    </a>
-                    <a href="#" class="team-card">
-                        <div class="team-card__img">
-                            <img src="<?php bloginfo('template_directory') ?>/img/team.jpeg" alt="">
-                        </div>
-                        <h3 class="team-card__title">
-                            Имя Отчество Фамилия
-                        </h3>
-                        <div class="team-card__description">
-                            Главный врач
-                        </div>
-                    </a>
-                    <a href="#" class="team-card">
-                        <div class="team-card__img">
-                            <img src="<?php bloginfo('template_directory') ?>/img/team.jpeg" alt="">
-                        </div>
-                        <h3 class="team-card__title">
-                            Имя Отчество Фамилия
-                        </h3>
-                        <div class="team-card__description">
-                            Главный врач
-                        </div>
-                    </a>
+                    <?php 
+                        $args = array(
+                            'numberposts' => -1,
+                            'orderby'     => 'date',
+                            'order'       => 'DESC',
+                            'include'     => array(),
+                            'exclude'     => array(),
+                            'meta_key'    => '',
+                            'meta_value'  =>'',
+                            'post_type'   => 'team',
+                            'suppress_filters' => true, 
+                        );
+                        $posts = get_posts( $args );
+                        foreach($posts as $post) { setup_postdata($post); ?>
+                            <a href="<?php the_permalink();?>" class="team-card">
+                                <div class="team-card__img">
+                                    <img src="<?php the_post_thumbnail_url(); ?>" alt="">
+                                </div>
+                                <h3 class="team-card__title">
+                                    <?php the_title();?>
+                                </h3>
+                                <div class="team-card__description">
+                                    <?php the_field('dolzhnost'); ?>
+                                </div>
+                            </a>
+                        <?php }
+                        wp_reset_postdata();          
+                    ?>
                 </div>
             </div>
         </section>
