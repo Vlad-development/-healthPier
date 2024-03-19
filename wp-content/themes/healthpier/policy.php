96bc5b6318ae20424f1cd3ae63b466681476f869
Vlad <dev@smart-ui.pro>
2024-03-19 02:24:53 +0200

    add policy

---
+++ wp-content/themes/healthpier/policy.php
@@ -0,0 +1,19 @@
+<?php 
+/*
+Template Name: Политика в отношении обработки персональных данных 
+Template Post Type:  page
+*/
+?>
+  
+   <?php get_header()?>
+
+   <main> 
+    <div class="container">
+        <h1 class="h1"><?php the_title();?></h1>
+
+        <?php the_content();?>
+    </div>
+
+   </main>
+    
+    <?php get_footer()?>
